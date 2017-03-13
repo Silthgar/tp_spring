4da235ac2986c57a6355d2c0c66c8ea2551dc95c
Erwan <erwan.gillion@gmail.com>
2017-03-13 11:16:34 +0100

    initial commit

---
+++ src/main/java/com/erwan/nikonikospring/NikoNikoSpringApplication.java
@@ -0,0 +1,12 @@
+package com.erwan.nikonikospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NikoNikoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NikoNikoSpringApplication.class, args);
+	}
+}
---
+++ src/test/java/com/erwan/nikonikospring/NikoNikoSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.erwan.nikonikospring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NikoNikoSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
